2cac2c9a98c1626aa2593eb4cb2dd163f8042bea
Sarina Chang <sarinachang7@gmail.com>
2022-01-14 14:53:50 -0500

    did pwd

--- bash.js
+++ bash.js
@@ -1,1 +1,13 @@
-asdfasdf
+process.stdout.write("prompt > ");
+
+process.stdin.on("data", (data) => {
+  const cmd = data.toString().trim();
+
+  if (cmd === "pwd") {
+    process.stdout.write(__dirname);
+    process.stdout.write("\nprompt > ");
+  } else {
+    process.stdout.write("error");
+    process.stdout.write("\nprompt > ");
+  }
+});
